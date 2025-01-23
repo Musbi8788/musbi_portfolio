c2e7487591ca2e20a29178df9fa5628e8caa589a
Musbi8788 <musbi700@gmail.com>
2025-01-23 20:29:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, redirect, request, url_for
+import smtplib
+
+
+my_email = "musbimusbi7@gmail.com"
+password = "wmtt zcdj zywa mctw"
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
+
+@app.route("/contact", methods=["GET","POST"])
+def contact():
+    if request.method == "POST" :
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+
+def send_email(name, email, phone, message):
+    send_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\n Phone: {phone}\n Message: {message}"
+    print(send_message)
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        server.starttls()
+        server.login(my_email, password)
+        server.sendmail(from_addr=my_email, to_addrs=email, msg=send_message)
+
+if __name__ == "__main__":
+    app.run(debug=True)
