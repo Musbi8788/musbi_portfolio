996a346702a3d48049fc7bdbca9b01cc8758b25c
Musbi <musbi700@gmail.com>
2025-07-03 19:57:20 +0100

    updated

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
 from flask import Flask, render_template, redirect, request, url_for
 import smtplib
-import os 
+import os
 
 
-my_email = "YOUR_EMAIL"
-password = "PASSWORD"
+my_email = os.environ.get("YOUR_EMAIL")
+password = os.environ.get("PASSWORD")
+print(my_email, password)
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 @app.route("/projects")
 def projects():
     return render_template("projects.html")
+
 
 @app.route("/resume")
 def resume():
     return render_template("resume.html")
 
-@app.route("/contact", methods=["GET","POST"])
+
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    if request.method == "POST" :
+    if request.method == "POST":
         data = request.form
         send_email(data["name"], data["email"], data["phone"], data["message"])
         return render_template("contact.html", msg_sent=True)
@@ -37,6 +42,7 @@ def send_email(name, email, phone, message):
         server.starttls()
         server.login(my_email, password)
         server.sendmail(from_addr=my_email, to_addrs=email, msg=send_message)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
